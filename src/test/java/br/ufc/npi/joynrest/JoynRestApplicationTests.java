922e47357d57b3a1d4ecaa0e5f7eff3603d19df1
Ianka Karuzia <iankakaruzia@hotmail.com>
2017-09-27 14:51:19 -0300

    Refatoração para Homologação

--- src/test/java/br/ufc/npi/joynrest/JoynRestApplicationTests.java
+++ src/test/java/br/ufc/npi/joynrest/JoynRestApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class JoynrestApplicationTests {
+public class JoynRestApplicationTests {
 
 	@Test
 	public void contextLoads() {
